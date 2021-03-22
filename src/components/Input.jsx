86a85f0009789b0e5efef8f65dd42e4b16f15c8a
Jaime Garcia <jaimegarciaamoros@gmail.com>
2021-03-22 22:05:30 +0100

    add some components

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div>
+            <form></form>
+            <input className='input' type="text" placeholder="Insert new task"></input>
+        </div>
+    )
+}
+
+export default Input
+
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <div>
+            <h2 className="task-list">Task List:</h2>
+            <ul></ul>
+        </div>
+    )
+}
+
+export default List
